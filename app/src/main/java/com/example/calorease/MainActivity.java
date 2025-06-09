979eca3e45f50b0d4e3779c7dcf93163d72128f6
yegezici <yegezici@gmail.com>
2025-06-09 20:04:42 +0300

    son updates

--- app/src/main/java/com/example/calorease/MainActivity.java
+++ app/src/main/java/com/example/calorease/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.calorease;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,11 +12,17 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    private SharedPreferences sharedPreferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sharedPreferences = getSharedPreferences("LoginPrefs", MODE_PRIVATE);
+        boolean isRemembered = sharedPreferences.getBoolean("isRemembered", false);
+        if (isRemembered) {
+            startActivity(new Intent(MainActivity.this, HomeActivity.class));
+            finish();
+        }
 
         // Edge-to-Edge (tam ekran) özellikleri
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
